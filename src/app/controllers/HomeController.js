5585171225019e63612d7201cfcb93c0ff2ae312
ngoquochieu <ngoquochieu06112001@gmail.com>
2021-09-27 23:47:58 +0700

    Read data from the database to the Home page

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,16 +1,19 @@
 const Items = require('../models/items');
 const Users = require('../models/users');
+const {multipleMongooseToObject} = require('../../util/mongoose');
 class HomeController {
     index(req, res, next) {
-        res.render('home', {
-            title: 'Home page',
-            style: 'home.css',
-            script: 'home.js',
-            nav: 'nav.css',
-        });
-        // Users.find()
-        //     .then(items => res.json(items))
-        //     .catch(next);
+        Items.find()
+            .then(items => {
+                res.render('home', {
+                    title: 'Home page',
+                    style: 'home.css',
+                    script: 'home.js',
+                    nav: 'nav.css',
+                    items:multipleMongooseToObject(items),
+                });
+            })
+            .catch(next);
     }
 }
 
