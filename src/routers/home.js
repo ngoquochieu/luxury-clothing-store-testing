714b401d021a156963a5dac37cf319332dd361c4
ngoquochieu <ngoquochieu06112001@gmail.com>
2021-10-16 23:59:11 +0700

    Update router / and /items

--- src/routers/home.js
+++ src/routers/home.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('../app/controllers/HomeController');
-const itemsController = require('../app/controllers/ItemsController')
-
-router.get('/:type', itemsController.showItemsOfCate);
+const itemsController = require('../app/controllers/ItemsController');
+const isAuthor = require('../app/middleware/Author');
 router.get('/', homeController.index);
 
 module.exports = router;
--- src/routers/items.js
+++ src/routers/items.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const itemsController = require('../app/controllers/ItemsController');
 
 router.get('/:type/details/:product_code', itemsController.detailsItems);
-router.get('/:type', itemsController.showItemsOfCate);  
+router.get('/:type', itemsController.showItemsOfCate);
 
 module.exports = router;
