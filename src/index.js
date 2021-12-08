5a11cae0205b4f187f05fa0dcf7eb603948ec061
ngoquochieu <ngoquochieu06112001@gmail.com>
2021-12-08 23:57:35 +0700

    add Error handle functon

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const logger = require('morgan');
 const handlebars = require('express-handlebars');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
@@ -6,10 +7,13 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const path = require('path');
 const route = require('./routers');
+const passport = require('passport');
+const  flash = require('connect-flash');
 const app = express();
 const db = require('./config/db/index');
 const port = 3001;
 
+// app.use(logger('dev'));
 //Connect db
 db.connect();
 app.use(express.json());
@@ -34,6 +38,11 @@ app.use(session({
     saveUninitialized:false,
     store: store,
 }))
+
+app.use(flash());
+app.use(passport.initialize())
+app.use(passport.session());
+
 // app.use(bcrypt());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'util')));
@@ -54,6 +63,27 @@ app.engine(
 );
 app.set('view engine', 'hbs');
 route(app);
+
+// Catch 404 Errors and forward them to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found'); 
+    console.log("")
+    err.status = 404;
+    next(err);
+})
+
+//Error handler function
+app.use((err, req, res, next) => {
+    const error = app.get('env') === 'development' ? err : {};
+    const status = err.status || 500;
+
+    //response to client
+    return res.status(status).json({
+        error: {
+            message: error.message,
+        }
+    });
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
