f59e6d014f7e81040d7ea1ea7a993fd3d5b54dc8
ngoquochieu <ngoquochieu06112001@gmail.com>
2021-12-09 22:27:33 +0700

    update code router action

--- src/routers/action.js
+++ src/routers/action.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const router = express.Router();
+// const router = express.Router();
+const router = require('express-promise-router')();
+
 const actionController = require('../app/controllers/ActionController');
+
 const validateCookie = require('../app/middleware/ValidateCookie');
 const author = require('../app/middleware/Author')
 
-router.get('/login', actionController.action);
-router.post('/login', actionController.login);
+router.route('/login')
+    .get(actionController.action)
+    .post(actionController.login);
+
 router.post('/signup', actionController.signup);
+
 router.post('/logout', actionController.logout);
 module.exports = router;
